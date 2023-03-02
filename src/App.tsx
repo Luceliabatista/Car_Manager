19f976a329ddd9a4859394c5e8177aa33fb80706
Lucélia Batista <lucelia.ti@gmail.com>
2023-03-02 11:07:55 -0300

    Adicionando os temas: claro e escuro, e provider MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
