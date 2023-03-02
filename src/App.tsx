c41d5933bef6968f541c10edc3c4bd6e089c2be1
Lucélia Batista <lucelia.ti@gmail.com>
2023-03-01 22:12:53 -0300

    Adicionando React Router DOM.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bem Vindo!</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
